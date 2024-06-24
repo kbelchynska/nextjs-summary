1aea2dadf3d836aed33ee5ca8816bce922445fed
Катерина Бельчинська <72660010+kbelchynska@users.noreply.github.com>
2024-06-24 12:49:11 +0000

    add pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Rome_skyline_panorama.jpg/1920px-Rome_skyline_panorama.jpg",
+    address: "@@@",
+    description: "!!!",
+  },
+  {
+    id: "m2",
+    title: "First meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Rome_skyline_panorama.jpg/1920px-Rome_skyline_panorama.jpg",
+    address: "@@@",
+    description: "!!!",
+  },
+];
+
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
