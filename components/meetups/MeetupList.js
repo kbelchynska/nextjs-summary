e827335cd67b2fe3b0600c4471ceaebb63d26c63
Катерина Бельчинська <72660010+kbelchynska@users.noreply.github.com>
2024-06-25 11:26:54 +0000

    data fetchings for static pages

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -4,15 +4,16 @@ import classes from "./MeetupList.module.css";
 function MeetupList(props) {
   return (
     <ul className={classes.list}>
-      {props.meetups.map((meetup) => (
-        <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-        />
-      ))}
+      {props.meetups &&
+        props.meetups.map((meetup) => (
+          <MeetupItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+          />
+        ))}
     </ul>
   );
 }
