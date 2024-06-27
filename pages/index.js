6349efb2c532b761a15468fc720035517e5cddc4
Катерина Бельчинська <72660010+kbelchynska@users.noreply.github.com>
2024-06-27 20:43:23 +0000

    add request function

--- pages/index.js
+++ pages/index.js
@@ -24,26 +24,26 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
-export async function getServerSideProps(context) {
-  const req = context.req;
-  const res = context.res;
-
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-  };
-}
-
-// export async function getStaticProps() {
-//   // fetch data
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
 
 //   return {
 //     props: {
 //       meetups: DUMMY_MEETUPS,
 //     },
-//     revalidate: 1,
 //   };
 // }
+
+export async function getStaticProps() {
+  // fetch data
+
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
+}
 
 export default HomePage;
