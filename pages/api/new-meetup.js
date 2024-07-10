a5cbb52d53d910077a51651fc8f3c4b898b7b320
Катерина Бельчинська <72660010+kbelchynska@users.noreply.github.com>
2024-07-10 19:45:35 +0000

    add form submit

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,23 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = MongoClient.connect(
+      "mongodb+srv://new_user:ARnxAFXv4a6kwzs6@cluster0.cphv3uh.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0",
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Message inserted!" });
   }
 }
 
